5b60b9066dac99819cdb0f3662cf437a6efa9370
ezu0 <burimofumofu@gmail.com>
2024-11-21 19:31:35 +0900

    routing機能について

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return 'Main Page';
 });
+
+Route::get('/hello', function () {
+  return 'Hello';
+})->name('hello');
+
+Route::get('/greet/{name}', function ($name) {
+  return "Hello, {$name}!";
+});
+
+Route::get('/hallo', function () {
+  return redirect('/hello')->route('hello');
+});
+
+Route::fallback(function () {
+  return 'Still got somewhere!';
+});
+
+
+//GET
+//POST
+//PUT
+//DELETE
