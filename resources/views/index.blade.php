a07b4ea7279a16604b901c68f1d630f7474e83c7
ezu0 <burimofumofu@gmail.com>
2024-11-22 19:42:12 +0900

    bladeファイルでのphp処理

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,15 @@
+<h2>
+  タスクの一覧
+</h2>
+
 <div>
-  Hello Im a blade template!
+  {{-- @if (count($tasks)) --}}
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+  @empty
+    <div>タスクがありません</div>
+  @endforelse
+  {{-- @endif --}}
 </div>
-@isset($name)
-  The name is: {{ $name }}
-@endisset
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,79 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+class Task
+{
+  public function __construct(
+    public int $id,
+    public string $title,
+    public string $description,
+    public ?string $long_description,
+    public bool $completed,
+    public string $created_at,
+    public string $updated_at
+  ) {}
+}
+
+$tasks = [
+  new Task(
+    1,
+    '週末の食材を買う',
+    '八百屋とドラスト、スーパーでそれぞれ買い物する',
+    '八百屋: トマト、ニンジン、大根、玉ねぎ、しょうが、ドラスト: 食器用洗剤、入浴剤、目薬、スーパー：厚揚げ、コンソメ、卵、納豆、豆乳、豆板醤',
+    false,
+    '2023-03-01 12:00:00',
+    '2023-03-01 12:00:00'
+  ),
+  new Task(
+    2,
+    '天使にラブソングを見る',
+    'またあのシスタージョークを浴びるため映画見る',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+  ),
+  new Task(
+    3,
+    'Laravelアプリを作成する',
+    '求人ポータルを作成してみる',
+    '多機能でかっこいいUIの求人ポータルを作成する',
+    true,
+    '2023-03-03 12:00:00',
+    '2023-03-03 12:00:00'
+  ),
+  new Task(
+    4,
+    '犬のさんぽ',
+    '散歩しないとジョンが悲しむ',
+    null,
+    false,
+    '2023-03-04 12:00:00',
+    '2023-03-04 12:00:00'
+  ),
+];
+
+Route::get('/', function () use ($tasks) {
   return view('index', [
-    'name' => 'Piort'
+    'tasks' => $tasks
   ]);
-});
+})->name('tasks.index');
 
-Route::get('/hello', function () {
-  return 'Hello';
-})->name('hello');
+Route::get('/{id}', function ($id) {
+  return 'One single task';
+})->name('tasks.show');
 
-Route::get('/greet/{name}', function ($name) {
-  return "Hello, {$name}!";
-});
+// Route::get('/hello', function () {
+//   return 'Hello';
+// })->name('hello');
 
-Route::get('/hallo', function () {
-  return redirect('/hello')->route('hello');
-});
+// Route::get('/greet/{name}', function ($name) {
+//   return "Hello, {$name}!";
+// });
+
+// Route::get('/hallo', function () {
+//   return redirect('/hello')->route('hello');
+// });
 
 Route::fallback(function () {
   return 'Still got somewhere!';
