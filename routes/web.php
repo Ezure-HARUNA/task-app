48fcbfb5b3b35b51c1a102d163436c5b0280b4a4
ezu0 <burimofumofu@gmail.com>
2024-11-24 15:53:18 +0900

    tinkerを使用したデータの読み取り

--- routes/web.php
+++ routes/web.php
@@ -2,76 +2,23 @@
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {}
-}
-
-$tasks = [
-  new Task(
-    1,
-    '週末の食材を買う',
-    '八百屋とドラスト、スーパーでそれぞれ買い物する',
-    '八百屋: トマト、ニンジン、大根、玉ねぎ、しょうが、ドラスト: 食器用洗剤、入浴剤、目薬、スーパー：厚揚げ、コンソメ、卵、納豆、豆乳、豆板醤',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    '天使にラブソングを見る',
-    'またあのシスタージョークを浴びるため映画見る',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Laravelアプリを作成する',
-    '求人ポータルを作成してみる',
-    '多機能でかっこいいUIの求人ポータルを作成する',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    '犬のさんぽ',
-    '散歩しないとジョンが悲しむ',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
 
 Route::get('/', function () {
   return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
   return view('index', [
-    'tasks' => $tasks
+    //MEMO: latest()->get()はcreated_atやupdated_atの降順(最新のものから順に)取得するall()メソッドみたいなもの
+    'tasks' => Task::latest()->get(),
   ]);
 })->name('tasks.index');
 
-Route::get('tasks/{id}', function ($id) use ($tasks) {
-  // return view('');
-  $task = collect($tasks)->firstWhere('id', $id);
-  if (!$task) {
-    abort(Response::HTTP_NOT_FOUND);
-  }
-  return view('show', ['task' => $task]);
+Route::get('tasks/{id}', function ($id) {
+
+  return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 // Route::get('/hello', function () {
