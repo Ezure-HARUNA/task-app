8801efc522b786ea883c1dea50229dfca39f5457
ezu0 <burimofumofu@gmail.com>
2024-11-25 18:04:39 +0900

    feat: タスクの更新

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'タスクの追加')
+@section('title', 'タスクの編集')
 
 @section('styles')
   <style>
@@ -12,31 +12,32 @@
 @endsection
 
 @section('content')
-  <form method="POST" action="{{ route('tasks.store') }}">
+  <form method="POST" action="{{ route('tasks.update', ['id' => $task->id]) }}">
     @csrf
+    @method('PUT')
     <div>
       <label for="title">タイトル</label>
-      <input type="text" name="title" id="title">
+      <input type="text" name="title" id="title" value="{{ $task->title }}">
       @error('title')
         <p class="error-message">{{ $message }}</p>
       @enderror
     </div>
     <div>
       <label for="description">概要</label>
-      <textarea name="description" rows="5"></textarea>
+      <textarea name="description" rows="5">{{ $task->description }}</textarea>
       @error('description')
         <p class="error-message">{{ $message }}</p>
       @enderror
     </div>
     <div>
       <label for="long_description">詳細</label>
-      <textarea name="long_description" rows="5"></textarea>
+      <textarea name="long_description" rows="5">{{ $task->long_description }}</textarea>
       @error('long_description')
         <p class="error-message">{{ $message }}</p>
       @enderror
     </div>
     <div>
-      <button type="submit">追加</button>
+      <button type="submit">編集</button>
     </div>
   </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,33 @@ Route::post('/tasks', function (Request $request) {
     ->with('success', 'タスクを登録しました');
 })->name('tasks.store');
 
+Route::put('/tasks/{id}', function ($id, Request $request) {
+  // dd($request->all());
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+  ]);
+  $task = Task::findOrFail($id);
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  $task->save();
+  return redirect()->route('tasks.show', ['id' => $task->id])
+    ->with('success', 'タスクを更新しました');
+})->name('tasks.update');
+
+Route::get('/tasks/{id}/edit', function ($id) {
+  return view('edit', [
+    'task' => Task::findOrFail($id)
+  ]);
+})->name('tasks.edit');
+
 Route::get('/tasks/{id}', function ($id) {
 
   return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
+
 
 
 
